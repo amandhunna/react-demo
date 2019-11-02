6c4b1b6972d23a338e089a0c5246d1191ced8e00
amanjotsingh <amanjotsingh260@gmail.com>
2019-11-02 23:36:11 +0530

    WIP

--- src/scenes/Login/Login.jsx
+++ src/scenes/Login/Login.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import Button from '../../Components/Button';
 import TextField from '../../Components/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 
 const Login = () => {
+
+    const useStyles = makeStyles(theme => ({
+        root: {
+            padding: theme.spacing(3, 2),
+        },
+    }));
+
+    const classes = useStyles();
+
     return (
         <div>
-            <TextField placeholder='username' />
-            <TextField password='password' />
-            <Button variant="contained">Login</Button>
+            <Paper className={classes.root}>
+                <TextField placeholder='username' />
+                <TextField password='password' />
+                <Button variant="contained">Login</Button>
+            </Paper>
         </div>);
 }
 
